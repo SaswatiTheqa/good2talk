260a162c64ed507aea4ce10a458295e05e186e23
saswati-qa <saswati@theqa.in>
2024-04-04 12:38:12 +0530

    Git Initate

---
+++ assets/js/mainframe.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    var swiper1 = new Swiper(".slider-students-feeling .swiper-container", {
+        slidesPerView: 3.6,
+        spaceBetween: 30,
+        // centeredSlides: true,
+        navigation: {
+            nextEl: '.slider-students-feeling .swiper-button-next',
+            prevEl: '.slider-students-feeling .swiper-button-prev'
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+                  0: { slidesPerView: 1, spaceBetween: 20, },
+                  440: { slidesPerView: 2, spaceBetween: 20, },
+                  575: { slidesPerView: 2.1, spaceBetween: 20, },
+                  767: { slidesPerView: 2.8, spaceBetween: 30, },
+                  991: { slidesPerView: 3.6, spaceBetween: 30, },
+                  1366: { slidesPerView: 3.6, spaceBetween: 30, },
+              }
+        
+      });
+    
+
+});
+
+
+
+
+
+
