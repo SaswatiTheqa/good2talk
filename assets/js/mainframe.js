c9cd4537d06e0dcac004a1401cb8b12ce0f8208c
saswati-qa <saswati@theqa.in>
2024-04-05 12:31:10 +0530

    html

--- assets/js/mainframe.js
+++ assets/js/mainframe.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // slider
     var swiper1 = new Swiper(".slider-students-feeling .swiper-container", {
         slidesPerView: 3.6,
         spaceBetween: 30,
@@ -16,12 +17,32 @@ $(document).ready(function () {
                   440: { slidesPerView: 2, spaceBetween: 20, },
                   575: { slidesPerView: 2.1, spaceBetween: 20, },
                   767: { slidesPerView: 2.8, spaceBetween: 30, },
-                  991: { slidesPerView: 3.6, spaceBetween: 30, },
+                  1024: { slidesPerView: 3.2, spaceBetween: 30, },
                   1366: { slidesPerView: 3.6, spaceBetween: 30, },
               }
         
       });
     
+    //   scrollTop
+    $("#scrollTop").click(function () {
+        $('html, body').animate({
+            scrollTop: $("header").offset().top
+        }, 1500);
+    });
+
+
+    //   provinceGo
+    const provinceGo = () => {
+        const selectVal = document.getElementById('provinceSelect').value;
+        if(selectVal !== ''){
+            window.open('_blank');
+        }
+    };
+    document.querySelector('#provinceGo').addEventListener('click', provinceGo);
+
+
+
+
 
 });
 
