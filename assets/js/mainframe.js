7bc032ce31e0bd812de91edcc48701da0b270d2c
unknown <saswati@theqa.in>
2024-04-08 16:15:00 +0530

    html

--- assets/js/mainframe.js
+++ assets/js/mainframe.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    // slider
+    // ===========   slider-students-feeling slider
     var swiper1 = new Swiper(".slider-students-feeling .swiper-container", {
         slidesPerView: 3.6,
         spaceBetween: 30,
@@ -22,6 +22,31 @@ $(document).ready(function () {
               }
         
       });
+
+    // ===========   insta-feed slider
+
+      var swiper1 = new Swiper(".insta-feed .swiper-container", {
+        slidesPerView: 3.02,
+        spaceBetween: 30,
+        // centeredSlides: true,
+        navigation: {
+            nextEl: '.insta-feed .swiper-button-next',
+            prevEl: '.insta-feed .swiper-button-prev'
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+                  0: { slidesPerView: 1.2, spaceBetween: 20, },
+                  440: { slidesPerView: 1.6, spaceBetween: 20, },
+                  575: { slidesPerView: 2.1, spaceBetween: 20, },
+                  767: { slidesPerView: 2.8, spaceBetween: 30, },
+                  1024: { slidesPerView: 3.2, spaceBetween: 30, },
+                  1366: { slidesPerView: 3.02, spaceBetween: 30, },
+              }
+        
+      });
     
     //   scrollTop
     $("#scrollTop").click(function () {
@@ -35,14 +60,10 @@ $(document).ready(function () {
     const provinceGo = () => {
         const selectVal = document.getElementById('provinceSelect').value;
         if(selectVal !== ''){
-            window.open('_blank');
+            window.location.href = selectVal+ '.html';
         }
     };
     document.querySelector('#provinceGo').addEventListener('click', provinceGo);
-
-
-
-
 
 });
 
